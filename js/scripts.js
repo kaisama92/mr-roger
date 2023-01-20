422372ac12fdcbe9aed24187e313369697ea9c6c
Kai Clausen <kaiclausen123@gmail.com>
2023-01-20 09:21:55 -0800

    create function countUpTo().

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
-function returnInput(input) {
-  
+//Utility Logic
+function returnInput(number) {
+  return parseInt(number);
 }
+
+//Business Logic 
+
+function countUpTo(number) {
+  let totalArray = [];
+  for (let currentNumber = 0; currentNumber <= number; currentNumber += 1) {
+    totalArray.push(parseInt(currentNumber))
+  }
+  const joinedTotalArray = totalArray.join(" ");
+  return joinedTotalArray;
+}
+
+
+//UI Logic
+
